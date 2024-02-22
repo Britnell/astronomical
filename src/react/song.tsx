006b02c8efdce6d7c6db0ca61ebef4c4f96fffe9
Britnell <tommy.britnell@gmail.com>
2024-02-22 21:03:33 +0100

    separate comps + base colours

---
+++ src/react/song.tsx
@@ -0,0 +1,161 @@
+import { useEffect, useRef, useState } from "react";
+import type { SamplesT } from "./player";
+import { SampleWave, Wave, findmax } from "./viz";
+import { audioContext, loadSource } from "./loader";
+
+const keybounce: { [id: string]: boolean } = {};
+
+export default function Song({
+  bufferId,
+  buffer,
+  callback,
+  removeBuffer,
+  samples,
+  edit,
+}: {
+  bufferId: string;
+  edit: string;
+  buffer: AudioBuffer;
+  callback: (type: string, val: any) => void;
+  samples: SamplesT;
+  removeBuffer: (id: string) => void;
+}) {
+  const [wavebuffer, setWavebuffer] = useState<number[] | null>(null);
+  const sources = useRef<{ [id: string]: AudioBufferSourceNode | null }>({});
+  const speed = 1.0;
+
+  useEffect(() => {
+    // calc wavebuffer for viz
+    const audioData = buffer.getChannelData(0);
+    const chunkSize = buffer.sampleRate / 1000;
+    const chunks = audioData.length / chunkSize;
+    let last = 0;
+
+    const wave = [];
+    for (let x = 1; x < chunks; x++) {
+      const to = Math.floor(chunkSize * x);
+      const slice = audioData.slice(last, to);
+      const max = findmax(slice);
+      wave.push(max);
+      last = to + 1;
+    }
+
+    setWavebuffer(wave);
+  }, [buffer]);
+
+  useEffect(() => {
+    // listen to keys & play samples
+    const keydown = (ev: KeyboardEvent) => {
+      const key = ev.key;
+
+      if (ev.repeat) return;
+      if (ev.ctrlKey) return; // avoid ctrl + f
+      if (keybounce[key]) return; // key being held
+
+      const sample = samples[key];
+      if (!sample?.active) return; // not a sample key
+      if (sample.bufferid !== bufferId) return; // not for this sample
+      // load & play
+      // const source = loadSource(buffer, speed);
+      sources.current[key]?.start(audioContext.currentTime, samples[key].begin);
+      keybounce[key] = true;
+    };
+
+    const keyup = ({ key }: KeyboardEvent) => {
+      const sample = samples[key];
+      if (!sample?.active) return;
+      if (sample.bufferid !== bufferId) return;
+      // stop
+      sources.current[key]?.stop();
+      // load next
+      const source = loadSource(buffer, speed);
+      sources.current[key] = source;
+      keybounce[key] = false;
+    };
+
+    window.addEventListener("keydown", keydown);
+    window.addEventListener("keyup", keyup);
+
+    return () => {
+      window.removeEventListener("keydown", keydown);
+      window.removeEventListener("keyup", keyup);
+    };
+  }, [samples, buffer]);
+
+  return (
+    <div className="song px-8 my-40">
+      <div>
+        {wavebuffer ? (
+          <Wave
+            buffer={buffer}
+            wave={wavebuffer}
+            onclick={(t) => callback("seek", t)}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+
+      <div className=" py-3 flex items-center justify-between">
+        <h2 className=" text-lg">{bufferId}</h2>
+
+        <label>
+          Add key :
+          <input
+            type="text"
+            className=" w-[100px] py-1 px-2  border border-black"
+            onChange={({ target }) => {
+              callback("addkey", {
+                key: target.value,
+                song: bufferId,
+              });
+              target.value = "";
+            }}
+            placeholder="add key"
+          />
+        </label>
+
+        <button onClick={() => removeBuffer(bufferId)}>remove</button>
+      </div>
+
+      <div className=" my-8 px-8 flex flex-col gap-3 ">
+        {Object.values(samples).map((sample) => {
+          const { key, begin, active, bufferid: songid } = sample;
+          if (!active) return null;
+          if (bufferId !== songid) return null;
+          const editing = edit === key;
+          return (
+            <div className=" flex gap-3" key={key}>
+              <div
+                className={
+                  " w-36 h-36 p-3 flex flex-col gap-2 " +
+                  (editing ? " bg-blue-300" : " bg-[var(--fg)]")
+                }
+              >
+                <div className=" flex items-end justify-between">
+                  <h2 className=" text-2xl">{key}</h2>
+                  <p>{begin.toPrecision(4)}s</p>
+                </div>
+                <button
+                  onClick={() => callback("editkey", key)}
+                  className=" grow  w-full bg-black bg-opacity-10 hover:bg-opacity-15 "
+                >
+                  {editing ? "done" : "edit"}
+                </button>
+                <div className="x">
+                  <button onClick={() => ({ type: "delete", val: key })}>
+                    x
+                  </button>
+                </div>
+              </div>
+              <div className=" grow ">
+                <SampleWave sample={sample} wave={wavebuffer} buffer={buffer} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/react/viz.tsx
@@ -0,0 +1,151 @@
+import {
+  useEffect,
+  useRef,
+  type MouseEventHandler,
+  useLayoutEffect,
+} from "react";
+import type { SampleT } from "./player";
+
+export function SampleWave({
+  sample,
+  wave,
+  buffer,
+}: {
+  sample: SampleT;
+  wave: number[] | null;
+  buffer: AudioBuffer;
+}) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    const parent = canvasRef.current.parentElement?.getBoundingClientRect();
+    if (!parent) return;
+    canvasRef.current.width = parent?.width;
+    canvasRef.current.height = parent?.height;
+  }, [wave]);
+
+  useEffect(() => {
+    if (!wave) return;
+    if (!canvasRef.current) return;
+
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+
+    // draf from wavepos - x
+    const perc = sample.begin / buffer.duration;
+    const samplepos = Math.floor(perc * wave.length);
+    const W = canvasRef.current.width;
+    const H = canvasRef.current.height;
+    const chunkSize = 10;
+
+    // begin drawing
+    ctx.clearRect(0, 0, W, H);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#000";
+    ctx.beginPath();
+
+    for (let x = 0; x < W; x++) {
+      const from = samplepos + x * chunkSize;
+      const slice = wave.slice(from, from + chunkSize);
+      const max = findmax(slice);
+      const y = H * (1 - max * 1.0);
+      if (x === 0) ctx.moveTo(x, y);
+      else ctx.lineTo(x, y);
+    }
+    ctx.stroke();
+  }, [wave, sample]);
+
+  if (!wave)
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className=" border border-[#87a2c7] bg-[#87a2c7] "
+      width="100"
+      height="100"
+      // onClick={canvasClick}
+    />
+  );
+}
+
+export function Wave({
+  buffer,
+  wave,
+  onclick,
+}: {
+  buffer: AudioBuffer;
+  wave: number[];
+  onclick: (t: number) => void;
+}) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const canvasClick: MouseEventHandler<HTMLCanvasElement> = (ev) => {
+    // if (!edit) return;
+    const cvs = ev.target as HTMLCanvasElement;
+    const perc = ev.clientX / cvs.getBoundingClientRect().width;
+    const val = perc * (buffer?.duration ?? 0);
+    onclick(val);
+  };
+
+  useLayoutEffect(() => {
+    if (!canvasRef.current) return;
+    canvasRef.current.width = window.innerWidth - 48;
+  }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    if (!wave) return;
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+
+    // p
+    const LEN = wave?.length;
+    const W = canvasRef.current.width;
+    const H = canvasRef.current.height;
+    const chunkSize = LEN / W;
+
+    // begin drawing
+    ctx.clearRect(0, 0, W, H);
+    let last = 0;
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "rgb(0 0 0)";
+    ctx.beginPath();
+
+    for (let x = 1; x < W; x++) {
+      const to = Math.floor(chunkSize * x);
+      const slice = wave.slice(last, to);
+
+      const max = findmax(slice);
+      const y = H * (1 - max * 1.0);
+      last = to + 1;
+      if (x === 0) ctx.moveTo(x, y);
+      else ctx.lineTo(x, y);
+    }
+    ctx.stroke();
+  }, [wave]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className=" border border-[var(--fg)] bg-[var(--fg)] "
+      width="800"
+      height="120"
+      onClick={canvasClick}
+    />
+  );
+}
+
+export const findmax = (arr: Float32Array | number[]) => {
+  let max = 0;
+  arr.forEach((v) => {
+    const x = Math.abs(v);
+    if (x > max) max = x;
+  });
+  return max;
+};
