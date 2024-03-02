9356802567b703c78e3ef50bb82aa37b39ed28fe
Britnell <tommy.britnell@gmail.com>
2024-03-02 18:25:17 +0100

    make grid react comp ...

--- src/react/grids.tsx
+++ src/react/grids.tsx
@@ -1,7 +1,3 @@
-/** @jsxImportSource @builder.io/qwik */
-
-import { component$ } from "@builder.io/qwik";
-
 type Movies = {
   overview: string;
   backdrop_path: string;
@@ -11,7 +7,7 @@ type Movies = {
   vote_average: number;
 };
 
-const Grids = component$(({ data }: { data: Movies[] }) => {
+export default function Grids({ data }: { data: Movies[] }) {
   return (
     <div>
       <h1>Hey-ho! welcome to grid camp</h1>
@@ -19,7 +15,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
       <section>
         <h2>1fr</h2>
         <p>3 growing cols `1fr` each, EASY</p>
-        <div class="grid gap-4 grid-cols-[repeat(3,1fr)] ">
+        <div className="grid gap-4 grid-cols-[repeat(3,1fr)] ">
           <Squares n={3} />
         </div>
       </section>
@@ -29,16 +25,18 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
         <p>
           {`BUT this can be messed up in some cases in the real world, depending on the cards' content and it's size, as 1fr only determines the growth of available area`}
         </p>
-        <div class="grid gap-4 grid-cols-[repeat(3,1fr)]  overflow-auto">
+        <div className="grid gap-4 grid-cols-[repeat(3,1fr)]  overflow-auto">
           <>
             {data.slice(0, 3).map((_mov, m) => {
               const mov = { ..._mov };
               if (m !== 2) mov.overview = "...";
               return (
-                <div class=" bg-gray-200 ">
-                  <MovieImg path={mov.poster_path} class=" " />
-                  <h3 class=" font-serif text-4xl font-bold">{mov.title}</h3>
-                  <p class=" whitespace-pre ">{mov.overview}</p>
+                <div className=" bg-gray-200 ">
+                  <MovieImg path={mov.poster_path} className=" " />
+                  <h3 className=" font-serif text-4xl font-bold">
+                    {mov.title}
+                  </h3>
+                  <p className=" whitespace-pre ">{mov.overview}</p>
                 </div>
               );
             })}
@@ -60,7 +58,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
           to actually get 3 cols with content we can use{" "}
           <code>repeat(3,minmax(0,1fr))</code> - thats what tailwind does!
         </p>
-        <div class="grid gap-4 grid-cols-[repeat(3,minmax(0,1fr))]  ">
+        <div className="grid gap-4 grid-cols-[repeat(3,minmax(0,1fr))]  ">
           <MovieCards data={data} n={3} />
         </div>
       </section>
@@ -68,7 +66,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
       <section>
         <h2>{`...minmax(100px,300px) ?`}</h2>
         <p>{`cols will take the max (available space) it can. e.g. 300px. only <300px do these cols shrink down until they hit 100px, and then the grid overflows `}</p>
-        <div class="grid gap-4 grid-cols-[repeat(3,minmax(100px,300px))] justify-center  ">
+        <div className="grid gap-4 grid-cols-[repeat(3,minmax(100px,300px))] justify-center  ">
           <MovieCards data={data} n={3} />
         </div>
       </section>
@@ -76,7 +74,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
       <section>
         <h2>{` sweet, can we do that automagically? `}</h2>
         <p>{` yes, it will automatically fill the grid with collumns of specified size (also adding empty columns)  `}</p>
-        <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] justify-center ">
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] justify-center ">
           <MovieCards data={data} n={3} />
         </div>
       </section>
@@ -85,7 +83,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
         <h2>{` and auto-fit? `}</h2>
         <p>{`generally for grid with many elements this behaves the same. but when there is leftover space, then content wins. 
         existing content is 'fit' into grid when it can, so when it's just 3 then they grow. this is more niche, generally go with auto-fill as default `}</p>
-        <div class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center ">
+        <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center ">
           <MovieCards data={data} n={3} />
         </div>
       </section>
@@ -94,7 +92,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
         <h2>so for a grid of many items?</h2>
         <code>`repeat(auto-fill,minmax(140px,1fr))`</code>
         <p>{` will fit in as many cols of min-width as it can. the rest space is grown into with 1fr  `}</p>
-        <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(140px,1fr))] justify-center ">
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(140px,1fr))] justify-center ">
           <MovieCards data={data} n={10} />
         </div>
       </section>
@@ -104,7 +102,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
         <code>`repeat(auto-fill,minmax(400px,450px))`</code>
         <p>{` minmax(...,500px) will generally fit as many cols of 500px as it can, without growing larger than that.`}</p>
         <p>{` the min value only comes into play when the grid has less than the max space available, now elements start shrinking and the min tells us when to stop shrinking. having a min of 400px though would create overflow on mobile, so we can just use 0 to shrink cols to all sizes. (unless we want a scrollable carousel) `}</p>
-        <div class="grid gap-4 grid-cols-[repeat(auto-fill,minmax(0,450px))] justify-center ">
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(0,450px))] justify-center ">
           <MovieCards data={data} n={4} />
         </div>
       </section>
@@ -112,8 +110,8 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
       <section>
         <h2>fit-content</h2>
         <p>{` this will grow up to a max of the value provided, and shrink down ao a min of 'min-content' . but im not sure if this same behaviour couldnt be achieved with a min. `}</p>
-        <div class="grid gap-4 grid-cols-[fit-content(300px)_1fr] ">
-          <p class="x">{data[0].overview}</p>
+        <div className="grid gap-4 grid-cols-[fit-content(300px)_1fr] ">
+          <p className="x">{data[0].overview}</p>
           <div>
             <MovieCards data={data} n={1} />
           </div>
@@ -127,13 +125,13 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
           <a href="https://www.youtube.com/watch?v=c13gpBrnGEw">Kevin Powell</a>
           {` using css grid to layout the pages main content. using col names we can define different line width, so it is easy to breakout content that is wider than the page. simple example to understand whats going on. `}
         </p>
-        <div class="grid-kevin-simple   ">
+        <div className="grid-kevin-simple   ">
           <>
             {data.slice(0, 2).map((mov, m) => (
               <>
                 <MovieImg
                   path={mov.poster_path}
-                  class={" w-full aspect-[3] object-cover screenwidth"}
+                  className={" w-full aspect-[3] object-cover screenwidth"}
                 />
                 <h3>{mov.title}</h3>
                 <p>{mov.overview}</p>
@@ -151,7 +149,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
             Kevin Powell's full fancy content grid formula{" "}
           </a>
         </p>
-        <div class="content-grid   ">
+        <div className="content-grid   ">
           <>
             {data.slice(0, 2).map((mov, m) => (
               <>
@@ -165,7 +163,7 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
                 <h3>{mov.title}</h3>
                 <p>{mov.overview}</p>
                 {m === 0 && (
-                  <div class="full-width bg-blue-500 font-serif text-7xl  text-white">
+                  <div className="full-width bg-blue-500 font-serif text-7xl  text-white">
                     <p>SUPER HIGHLIGHT QUOTE</p>
                   </div>
                 )}
@@ -176,26 +174,26 @@ const Grids = component$(({ data }: { data: Movies[] }) => {
       </section>
     </div>
   );
-});
+}
 
 const MovieImg = (props: { path: string; class?: string }) => (
   <img
     src={`https://image.tmdb.org/t/p/w300/${props.path}`}
     alt="movie post"
-    class={props.class}
+    className={props.class}
   />
 );
 
 const MovieCards = ({ data, n = 20 }: { data: Movies[]; n?: number }) => (
   <>
     {data.slice(0, n).map((mov, m) => (
-      <div class=" bg-gray-200 ">
+      <div className=" bg-gray-200 ">
         <MovieImg
           path={mov.poster_path}
-          class=" w-full aspect-video object-cover "
+          className=" w-full aspect-video object-cover "
         />
-        <h3 class=" font-serif text-4xl font-bold">{mov.title}</h3>
-        <p class=" ">{mov.overview}</p>
+        <h3 className=" font-serif text-4xl font-bold">{mov.title}</h3>
+        <p className=" ">{mov.overview}</p>
       </div>
     ))}
   </>
@@ -206,9 +204,7 @@ const Squares = ({ n = 8 }: { n: number }) => (
     {Array(n)
       .fill(0)
       .map((_, i) => (
-        <div class=" bg-blue-200 aspect-square"></div>
+        <div className=" bg-blue-200 aspect-square"></div>
       ))}
   </>
 );
-
-export default Grids;
