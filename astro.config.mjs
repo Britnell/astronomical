8b5758d0e4adc8f47a4ce04320d212dcd4ade342
Britnell <tommy.britnell@gmail.com>
2024-03-03 11:35:35 +0100

    rmv vue...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
 import node from "@astrojs/node";
 
 // https://astro.build/config
@@ -10,9 +9,6 @@ export default defineConfig({
   integrations: [
     qwikdev({
       include: ["src/qwik/*"],
-    }),
-    vue({
-      include: ["src/vue/*"],
     }),
     react({
       include: ["src/react/*"],
